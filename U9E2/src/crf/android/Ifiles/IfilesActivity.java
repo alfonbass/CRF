bc759424f82219ce990da09c6b61008fb98bcc87
Alfon Delicado <alfonbass@gmail.com>
2012-04-26 19:52:51 +0200

    second_update

--- U9E2/src/crf/android/Ifiles/IfilesActivity.java
+++ U9E2/src/crf/android/Ifiles/IfilesActivity.java
@@ -2,14 +2,13 @@ package crf.android.Ifiles;
 
 import android.app.Activity;
 import android.content.Context;
-import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.preference.EditTextPreference;
-import android.preference.PreferenceManager;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
+import android.content.SharedPreferences;
 
 public class IfilesActivity extends Activity {
 	
@@ -17,39 +16,27 @@ public class IfilesActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
         
-        final String filename = "file.txt";
+        final EditText string = (EditText) findViewById(R.id.texto);
     	final Context context = this;
-    	final SharedPreferences preferencia = null;
-    	final EditTextPreference editTextPreference= 
         
         Button guardar = (Button) findViewById(R.id.guardar);
         guardar.setOnClickListener(new OnClickListener() {
 			public void onClick(View v) {	
-				
-				SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(context);
-				SharedPreferences.Editor editor = preferencia.edit();
-				editor.putString(filename,"valor_a_guardar");
+				String text=string.getText().toString();
+
+				SharedPreferences sharedpreferences = getSharedPreferences("datos", MODE_PRIVATE);
+				SharedPreferences.Editor editor = sharedpreferences.edit();
+				editor.putString("texto",text );
 				editor.commit();
 		        Toast.makeText(context, "Guardado", Toast.LENGTH_LONG).show();
 			}
 		});
         Button obtener = (Button) findViewById(R.id.obtener);
         obtener.setOnClickListener(new OnClickListener() {
-			public void onClick(View v) {
-				
-				String contenido = "";
-				/*FileInputStream fileInputStream;
-				try {
-				    fileInputStream = context.openFileInput(filename);
-				    byte [] InBuf = new byte[fileInputStream.available()];
-				    fileInputStream.read(InBuf);
-				    contenido = new String(InBuf);
-				} catch (FileNotFoundException e) {
-				    e.printStackTrace();
-				} catch (IOException e) {
-				    e.printStackTrace();
-				}*/
-				Toast.makeText(context, contenido, Toast.LENGTH_LONG).show();
+			public void onClick(View v) {				
+				SharedPreferences sharedpreferences = getSharedPreferences("datos", MODE_PRIVATE);
+				String text = sharedpreferences.getString("texto", "No se ha encontrado nada");
+				Toast.makeText(context, text, Toast.LENGTH_LONG).show();
 			}
         });
     }
